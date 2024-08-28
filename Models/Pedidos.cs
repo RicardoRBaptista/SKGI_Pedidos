5a8e18b150c3cea1e749e8ec3540361de41afa3c
Ricardo <elokaizen@hotmail.com>
2024-08-28 10:34:32 -0300

    SQL

--- Models/Pedidos.cs
+++ Models/Pedidos.cs
@@ -4,13 +4,13 @@ namespace SKGI_Pedidos.Models
 {
     public class Pedidos
     {
-        public Pedidos(string pedido, DateTime emissao, string status, int nF, DateTime coleta, string transportadora)
+        public Pedidos(string pedido, DateTime emissao, string status, int nF, DateTime date, string transportadora)
         {
             Pedido = pedido;
             Emissao = emissao;
             Status = status;
             NF = nF;
-            Coleta = coleta;
+            this.date = date;
             Transportadora = transportadora;
         }
 
@@ -18,7 +18,7 @@ namespace SKGI_Pedidos.Models
         public DateTime Emissao { get; set; }
         public string Status { get; set; }
         public int NF { get; set; }
-        public DateTime Coleta { get; set; }
+        public DateTime date { get; set; }
         public string Transportadora { get; set; }
     }
 }
